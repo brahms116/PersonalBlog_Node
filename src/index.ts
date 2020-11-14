276e5699f156aee5799f9b869249efe3f71f90b3
David Kwong <20544dk@gmail.com>
2020-11-14 12:20:53 +1000

    setup for gql

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express'
-import express_graphql from 'express-graphql'
+import {graphqlHTTP} from 'express-graphql'
 import {buildSchema} from 'graphql'
 import dotenv from 'dotenv'
 
+const schema = buildSchema(`
+    type Query{
+        message:String
+    }
+`)
+
+const root ={
+    message:()=>"hello world"
+}
 const app = express()
 dotenv.config()
+
+app.use("/graphql",graphqlHTTP({
+    schema:schema,
+    rootValue: root,
+    graphiql:true
+}))
+
+
 app.listen(process.env.PORT as string,()=>{
     console.log(`Server started on port ${process.env.PORT}`)
 }
